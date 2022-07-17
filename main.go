284075648cc9d837eb8164465b439e8ca83a68bc
Luis Guillermo Ramírez <lgramirezdiaz@gmail.com>
2022-07-17 15:05:23 -0600

    fixing cors

--- main.go
+++ main.go
@@ -32,7 +32,7 @@ var API_URL string
 
 func main() {
     router := gin.Default()
-	router.Use(CORSMiddleware())
+	router.Use(CORS)
     docs.SwaggerInfo.BasePath = "/api/v1"
     v1 := router.Group("/api/v1")
     API_HOST = os.Getenv("API_HOST")
@@ -61,21 +61,28 @@ func Helloworld(g *gin.Context)  {
     g.JSON(http.StatusOK,"Ok")
  }
 
- func CORSMiddleware() gin.HandlerFunc {
-    return func(c *gin.Context) {
+ func CORS(c *gin.Context) {
 
-        c.Header("Access-Control-Allow-Origin", "*")
-        c.Header("Access-Control-Allow-Credentials", "true")
-        c.Header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
-        c.Header("Access-Control-Allow-Methods", "POST,HEAD,PATCH, OPTIONS, GET, PUT")
+	// First, we add the headers with need to enable CORS
+	// Make sure to adjust these headers to your needs
+	c.Header("Access-Control-Allow-Origin", "*")
+	c.Header("Access-Control-Allow-Methods", "*")
+	c.Header("Access-Control-Allow-Headers", "*")
+	c.Header("Content-Type", "application/json")
 
-        if c.Request.Method == "OPTIONS" {
-            c.AbortWithStatus(204)
-            return
-        }
+	// Second, we handle the OPTIONS problem
+	if c.Request.Method != "OPTIONS" {
+		
+		c.Next()
 
-        c.Next()
-    }
+	} else {
+        
+		// Everytime we receive an OPTIONS request, 
+		// we just return an HTTP 200 Status Code
+		// Like this, Angular can now do the real 
+		// request using any other method than OPTIONS
+		c.AbortWithStatus(http.StatusOK)
+	}
 }
 
 // getVaccines responds with the list of all covid vaccines as JSON.
